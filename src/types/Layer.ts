6a6c48d7d7b4d7250313a1c27959df73b898ff71
Ben Rollin <ben.rollin@clever.com>
2023-05-21 02:19:00 -0700

    new layer abstraction

--- src/types/Layer.ts
+++ src/types/Layer.ts
@@ -5,62 +5,13 @@ import { UIStore } from "@/src/types/UIStore";
 import { binarySearchForBlockAtTime } from "@/src/utils/algorithm";
 import { DEFAULT_BLOCK_DURATION } from "@/src/utils/time";
 import { patterns } from "@/src/patterns/patterns";
-import { makeAutoObservable, configure } from "mobx";
+import { makeAutoObservable, configure, runInAction } from "mobx";
 import { AudioStore } from "@/src/types/AudioStore";
 import { Variation } from "@/src/types/Variations/Variation";
 import { ExperienceStore } from "@/src/types/ExperienceStore";
-import { Layer } from "@/src/types/Layer";
 
-// Enforce MobX strict mode, which can make many noisy console warnings, but can help use learn MobX better.
-// Feel free to comment out the following if you want to silence the console messages.
-configure({
-  enforceActions: "always",
-  computedRequiresReaction: true,
-  reactionRequiresObservable: true,
-  observableRequiresReaction: false, // This will trigger false positives sometimes, so turning off
-});
-
-export class Store {
-  initialized = false;
-  timer = new Timer();
-  uiStore = new UIStore();
-  audioStore = new AudioStore(this.timer);
-  experienceStore = new ExperienceStore(this);
-
-  layers: Layer[] = [new Layer()];
-
+export class Layer {
   patternBlocks: Block[] = [];
-  selectedBlocks: Set<Block> = new Set();
-
-  selectedVariationBlock: Block | null = null;
-  selectedVariationUniformName: string = "";
-  selectedVariation: Variation | null = null;
-
-  patterns: Pattern[] = patterns;
-  selectedPattern: Pattern = patterns[0];
-  draggingPattern: boolean = false;
-
-  _lastComputedCurrentBlock: Block | null = null;
-
-  // returns the block that the global time is inside of, or null if none
-  // runs every frame, so we keep this performant with caching + a binary search
-  get currentBlock(): Block | null {
-    if (
-      this._lastComputedCurrentBlock &&
-      this._lastComputedCurrentBlock.startTime <= this.timer.globalTime &&
-      this.timer.globalTime < this._lastComputedCurrentBlock.endTime
-    ) {
-      return this._lastComputedCurrentBlock;
-    }
-
-    const currentBlockIndex = binarySearchForBlockAtTime(
-      this.patternBlocks,
-      this.timer.globalTime
-    );
-    this._lastComputedCurrentBlock =
-      this.patternBlocks[currentBlockIndex] ?? null;
-    return this._lastComputedCurrentBlock;
-  }
 
   get endTime() {
     if (this.patternBlocks.length === 0) return 0;
@@ -69,71 +20,17 @@ export class Store {
     return lastBlock.endTime;
   }
 
-  _user = "";
-
-  get user(): string {
-    return this._user;
-  }
-
-  set user(value: string) {
-    this._user = value;
-    localStorage.setItem("user", value);
-  }
-
-  _experienceName = "untitled";
-
-  get experienceName(): string {
-    return this._experienceName;
-  }
-
-  set experienceName(value: string) {
-    this._experienceName = value;
-    localStorage.setItem("experienceName", value);
-  }
-
-  experienceLastSavedAt = 0;
-
   constructor() {
-    makeAutoObservable(this, {
-      _lastComputedCurrentBlock: false, // don't make this observable, since it's just a cache
-    });
+    makeAutoObservable(this);
+
+    runInAction(this.initialize);
   }
 
   initialize = () => {
-    this.experienceStore.loadInitialExperience();
-
-    // check for a username in local storage
-    const username = localStorage.getItem("user");
-    if (username) this._user = username;
-
-    // set up an autosave interval
-    setInterval(() => {
-      if (!this.timer.playing)
-        this.experienceStore.saveToLocalStorage("autosave");
-    }, 60 * 1000);
-
-    this.initialized = true;
-  };
-
-  insertCloneOfPattern = (pattern: Pattern) => {
-    const newBlock = new Block(pattern.clone());
-    const nextGap = this.nextFiniteGap(this.timer.globalTime);
-    newBlock.setTiming(nextGap);
+    // temporarily hard code a block
+    const newBlock = new Block(patterns[5].clone());
+    newBlock.setTiming({ startTime: 0, duration: 360 });
     this.addBlock(newBlock);
-  };
-
-  /**
-   * Inserts a clone of the given effect into the selected blocks
-   *
-   * @param {Pattern} effect
-   * @memberof Store
-   */
-  insertCloneOfEffect = (effect: Pattern) => {
-    if (this.selectedBlocks.size === 0) return;
-
-    for (const block of Array.from(this.selectedBlocks)) {
-      block.addCloneOfEffect(effect);
-    }
   };
 
   addBlock = (block: Block) => {
@@ -151,7 +48,7 @@ export class Store {
 
   removeBlock = (block: Block) => {
     this.patternBlocks = this.patternBlocks.filter((b) => b !== block);
-    this._lastComputedCurrentBlock = null;
+    // this._lastComputedCurrentBlock = null;
   };
 
   /**
@@ -169,91 +66,6 @@ export class Store {
   reorderBlock = (block: Block) => {
     this.removeBlock(block);
     this.addBlock(block);
-  };
-
-  selectBlock = (block: Block) => {
-    this.selectedBlocks = new Set([block]);
-  };
-
-  addBlockToSelection = (block: Block) => {
-    this.selectedBlocks.add(block);
-  };
-
-  deselectBlock = (block: Block) => {
-    this.selectedBlocks.delete(block);
-  };
-
-  selectAllBlocks = () => {
-    this.selectedBlocks = new Set(this.patternBlocks);
-  };
-
-  deselectAllBlocks = () => {
-    if (this.selectedBlocks.size === 0) return;
-    this.selectedBlocks = new Set();
-  };
-
-  deleteSelected = () => {
-    if (this.selectedBlocks.size > 0) {
-      Array.from(this.selectedBlocks).forEach(this.removeBlock);
-      this.selectedBlocks = new Set();
-      return;
-    }
-
-    if (this.selectedVariation) {
-      this.selectedVariationBlock?.removeVariation(
-        this.selectedVariationUniformName,
-        this.selectedVariation
-      );
-    }
-  };
-
-  copyBlocksToClipboard = (clipboardData: DataTransfer) => {
-    clipboardData.setData(
-      "text/plain",
-      JSON.stringify(Array.from(this.selectedBlocks).map((b) => b.serialize()))
-    );
-  };
-
-  pasteBlocksFromClipboard = (clipboardData: DataTransfer) => {
-    const blocksData = JSON.parse(clipboardData.getData("text/plain"));
-    if (!blocksData || !blocksData.length) return;
-
-    const blocksToPaste = blocksData.map((b: any) => Block.deserialize(b));
-    this.selectedBlocks = new Set();
-    for (const blockToPaste of blocksToPaste) {
-      const nextGap = this.nextFiniteGap(
-        this.timer.globalTime,
-        blockToPaste.duration
-      );
-      blockToPaste.setTiming(nextGap);
-      this.addBlock(blockToPaste);
-      this.addBlockToSelection(blockToPaste);
-    }
-  };
-
-  duplicateSelected = () => {
-    if (this.selectedBlocks.size > 0) {
-      const selectedBlocks = Array.from(this.selectedBlocks);
-      this.selectedBlocks = new Set();
-      for (const selectedBlock of selectedBlocks) {
-        const newBlock = selectedBlock.clone();
-        const nextGap = this.nextFiniteGap(
-          selectedBlock.endTime,
-          selectedBlock.duration
-        );
-        newBlock.setTiming(nextGap);
-        this.addBlock(newBlock);
-        this.addBlockToSelection(newBlock);
-      }
-      return;
-    }
-
-    if (this.selectedVariation) {
-      this.selectedVariationBlock?.duplicateVariation(
-        this.selectedVariationUniformName,
-        this.selectedVariation
-      );
-    }
   };
 
   /**
@@ -439,34 +251,13 @@ export class Store {
     block.duration += delta;
   };
 
-  selectVariation = (
-    block: Block,
-    uniformName: string,
-    variation: Variation
-  ) => {
-    if (this.selectedVariation?.id === variation.id) {
-      this.selectedVariation = null;
-      this.selectedVariationUniformName = "";
-      this.selectedVariationBlock = null;
-      return;
-    }
-
-    this.selectedVariation = variation;
-    this.selectedVariationUniformName = uniformName;
-    this.selectedVariationBlock = block;
-  };
-
   serialize = () => ({
-    audioStore: this.audioStore.serialize(),
-    blocks: this.patternBlocks.map((b) => b.serialize()),
-    uiStore: this.uiStore.serialize(),
-    user: this.user,
+    patternBlocks: this.patternBlocks.map((b) => b.serialize()),
   });
 
   deserialize = (data: any) => {
-    this.audioStore.deserialize(data.audioStore);
-    this.uiStore.deserialize(data.uiStore);
-    this.patternBlocks = data.blocks.map((b: any) => Block.deserialize(b));
-    this._lastComputedCurrentBlock = null;
+    this.patternBlocks = data.patternBlocks.map((b: any) =>
+      Block.deserialize(b)
+    );
   };
 }
