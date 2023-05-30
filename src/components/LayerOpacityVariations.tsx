b84416a5a41b639cbd35757710afa54d91a6ffd1
Ben Rollin <ben.rollin@clever.com>
2023-05-29 18:18:54 -0700

    add layer opacity variations component

---
+++ src/components/LayerOpacityVariations.tsx
@@ -0,0 +1,45 @@
+import { observer } from "mobx-react-lite";
+import { HStack, Text, VStack } from "@chakra-ui/react";
+import { Layer } from "@/src/types/Layer";
+import { NewVariationButtons } from "@/src/components/NewVariationButtons";
+import { ParameterVariations } from "@/src/components/ParameterVariations";
+
+type Props = {
+  layer: Layer;
+};
+
+export const LayerOpacityVariations = observer(function LayerOpacityVariations({
+  layer,
+}: Props) {
+  return layer.showingOpacityControls ? (
+    <VStack
+      position="absolute"
+      bottom={0}
+      left={150}
+      width="100%"
+      height="80px"
+      justify="center"
+      align="flex-start"
+      bgColor="blue.800"
+      borderTopWidth={1}
+      borderColor="gray.300"
+    >
+      {layer.opacityBlock.parameterVariations["u_opacity"]?.length ?? 0 > 0 ? (
+        <ParameterVariations
+          uniformName={"u_opacity"}
+          block={layer.opacityBlock}
+        />
+      ) : (
+        <HStack ml={2}>
+          <Text py={2} fontSize={10}>
+            Click to add a variation:
+          </Text>
+          <NewVariationButtons
+            uniformName={"u_opacity"}
+            block={layer.opacityBlock}
+          />
+        </HStack>
+      )}
+    </VStack>
+  ) : null;
+});
