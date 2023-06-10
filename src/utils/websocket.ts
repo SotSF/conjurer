97d05d6ff8fd50b88b6d66d07798a8bc2729341f
Ben Rollin <ben.rollin@clever.com>
2023-06-10 16:03:41 -0700

    better console logging of websocket connecting

--- src/utils/websocket.ts
+++ src/utils/websocket.ts
@@ -3,15 +3,27 @@ import { WEBSOCKET_HOST, WEBSOCKET_PORT } from "@/src/utils/websocketHost";
 let _websocket: WebSocket;
 
 export const setupWebsocket = () => {
+  console.log(
+    "Reconnecting to websocket server at",
+    WEBSOCKET_HOST,
+    WEBSOCKET_PORT
+  );
   _websocket = new WebSocket(`ws://${WEBSOCKET_HOST}:${WEBSOCKET_PORT}`);
   _websocket.binaryType = "arraybuffer";
 };
 
-setupWebsocket();
-
+let lastWarned = 0;
 export const transmitData = (data: Uint8Array) => {
+  if (!_websocket) {
+    setupWebsocket();
+    return;
+  }
+
   if (_websocket.readyState !== _websocket.OPEN) {
-    console.warn("Websocket not open, not sending data");
+    if (Date.now() - lastWarned > 5000) {
+      console.warn("Websocket not open, not sending data");
+      lastWarned = Date.now();
+    }
     return;
   }
 
