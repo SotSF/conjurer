41b69e8a127ad2d529cbe652e2f718b8bc91ddb3
Ben Rollin <ben.rollin@clever.com>
2023-06-13 00:23:21 -0700

    add CloudsMask effect

---
+++ src/effects/CloudsMask.ts
@@ -0,0 +1,23 @@
+import { Pattern } from "@/src/types/Pattern";
+import cloudsMask from "./shaders/cloudsMask.frag";
+
+export { cloudsMask };
+export const CloudsMask = () =>
+  new Pattern("Clouds Mask", cloudsMask, {
+    u_scale: {
+      name: "Scale",
+      value: 0.5,
+    },
+    u_speed: {
+      name: "Speed",
+      value: 1,
+    },
+    u_high_pass: {
+      name: "High Pass",
+      value: 0,
+    },
+    u_threshold_size: {
+      name: "Threshold Size",
+      value: 0.15,
+    },
+  });
