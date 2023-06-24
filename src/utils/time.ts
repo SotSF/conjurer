621a7b168d8a497de8cea51766daccf38eee488b
Ben Rollin <ben.rollin@clever.com>
2023-06-24 03:18:49 -0700

    use setTimeout instead of requestAnimationFrame

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,3 +3,5 @@ export const DEFAULT_VARIATION_DURATION = 2;
 export const MAX_TIME = 60 * 8; // length of the timeline in minutes
 export const INITIAL_PIXELS_PER_SECOND = 40;
 export const MINIMUM_VARIATION_DURATION = 0.1;
+
+export const MAX_FRAMES_PER_SECOND = 60;
