94164f36827b086d6e68dd039e717354fc1096e1
Justyn Pham <justynaipham@gmail.com>
2023-07-12 09:32:37 -0700

    change defaults; use cartesian mapping from globules.frag

--- src/patterns/Lissajous.ts
+++ src/patterns/Lissajous.ts
@@ -8,19 +8,19 @@ export const Lissajous = () =>
   new Pattern("Lissajous", lissajous, {
     u_count: {
         name: "Count",
-        value: 3
+        value: 8
     },
-    u_scale: {
-        name: "Size",
-        value: 0.5
+    u_space: {
+      name: "Interval",  
+      value: 0.05
     },
     u_a0: {
         name: "A",
-        value: 2
+        value: 1
     },
     u_a1: {
         name: "a",
-        value: 2.5
+        value: 2
     },
     u_b0: {
         name: "B",
@@ -28,15 +28,15 @@ export const Lissajous = () =>
     },
     u_b1: {
         name: "b",
-        value: 1
+        value: 3
     },
     u_palette: {
         name: "Palette",
         value: new Palette(
-            new Vector3(0.518,0.558,-0.112),
-            new Vector3(1.123,-1.082,1.288),
-            new Vector3(1.278,-0.332,-0.172),
-            new Vector3(-0.192,0.308,0.968)
+            new Vector3(0.5,0.5,0.5),
+            new Vector3(0.5,0.5,0.194),
+            new Vector3(1.0,0.608,1.0),
+            new Vector3(0.468,0.198,0.557)
           )
     }
   });
