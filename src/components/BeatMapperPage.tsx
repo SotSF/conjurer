f28917021f6ba5caaae96d85e6d4ea280aa390be
Ben Rollin <ben.rollin@clever.com>
2023-09-17 00:55:54 -0700

    editable threshold

--- src/components/BeatMapperPage.tsx
+++ src/components/BeatMapperPage.tsx
@@ -6,6 +6,7 @@ import {
   TableContainer,
   Tbody,
   Td,
+  Text,
   Th,
   Thead,
   Tr,
@@ -24,6 +25,7 @@ import {
   groupNeighborsByTempo,
 } from "@/src/utils/audio";
 import { runInAction } from "mobx";
+import { ScalarInput } from "@/src/components/ScalarInput";
 
 type TempoCount = {
   tempo: number;
@@ -33,11 +35,16 @@ type TempoCount = {
 export const BeatMapperPage = observer(function BeatMapperPage() {
   const store = useStore();
   const { uiStore, audioStore, playlistStore } = store;
+
+  const initialized = useRef(false);
   const timelineRef = useRef<HTMLDivElement>(null);
 
   useWheelZooming(timelineRef.current);
 
-  const initialized = useRef(false);
+  const [audioBuffer, setAudioBuffer] = useState<AudioBuffer | null>(null);
+  const [beats, setBeats] = useState<number[]>([]);
+  const [tempoCounts, setTempoCounts] = useState<TempoCount[]>([]);
+  const [threshold, setThreshold] = useState((0.8).toString());
 
   useEffect(() => {
     if (!initialized.current) {
@@ -50,76 +57,73 @@ export const BeatMapperPage = observer(function BeatMapperPage() {
     }
   }, [store, playlistStore, uiStore]);
 
-  const [beats, setBeats] = useState<number[]>([]);
-  const [tempoCounts, setTempoCounts] = useState<TempoCount[]>([]);
-
   useEffect(() => {
     audioStore.wavesurfer?.on("ready", () => {
       runInAction(() => {
         uiStore.pixelsPerSecond = 20;
       });
       const buffer = audioStore.wavesurfer!.getDecodedData()!;
-      console.log("buffer", buffer);
-      // Create offline context
-      var offlineContext = new OfflineAudioContext(
-        1,
-        buffer.length,
-        buffer.sampleRate
-      );
-
-      // Create buffer source
-      var source = offlineContext.createBufferSource();
-      source.buffer = buffer;
-
-      // Create filter
-      var filter = offlineContext.createBiquadFilter();
-      filter.type = "lowpass";
-      console.log("filter frequency", filter.frequency);
-
-      // Pipe the song into the filter, and the filter into the offline context
-      source.connect(filter);
-      filter.connect(offlineContext.destination);
-
-      // Schedule the song to start playing at time:0
-      source.start(0);
-
-      // Render the song
-      offlineContext.startRendering();
-
-      // Act on the result
-      offlineContext.oncomplete = function (e) {
-        // Filtered buffer!
-        var filteredBuffer = e.renderedBuffer;
-        console.log("filteredBuffer", filteredBuffer);
-        const filteredPeaks = getPeaksAtThreshold(
-          filteredBuffer.getChannelData(0),
-          0.8,
-          filteredBuffer.sampleRate
-        );
-        console.log("filteredPeaks", filteredPeaks);
-
-        const newBeats = filteredPeaks.map(
-          (peakIndex) => peakIndex / filteredBuffer.sampleRate
-        );
-        setBeats(newBeats);
-        const histogram = countIntervalsBetweenNearbyPeaks(filteredPeaks, 200);
-        // console.log("histogram", histogram);
-
-        const newTempoCounts = groupNeighborsByTempo(
-          histogram,
-          filteredBuffer.sampleRate
-        );
-        console.log("bpmCounts", newTempoCounts);
-
-        // sort data by highest count
-        const sortedTempoCounts = newTempoCounts.sort(
-          (a, b) => b.count - a.count
-        );
-        setTempoCounts(sortedTempoCounts);
-        console.log("sortedBpmCounts", sortedTempoCounts);
-      };
+      setAudioBuffer(buffer);
     });
   }, [audioStore.wavesurfer, audioStore.peaks, uiStore]);
+
+  useEffect(() => {
+    if (Number.isNaN(Number(threshold)) || !audioBuffer) return;
+
+    // Create offline context
+    var offlineContext = new OfflineAudioContext(
+      1,
+      audioBuffer.length,
+      audioBuffer.sampleRate
+    );
+
+    // Create buffer source
+    var source = offlineContext.createBufferSource();
+    source.buffer = audioBuffer;
+
+    // Create filter
+    var filter = offlineContext.createBiquadFilter();
+    filter.type = "lowpass";
+    console.log("filter frequency", filter.frequency);
+
+    // Pipe the song into the filter, and the filter into the offline context
+    source.connect(filter);
+    filter.connect(offlineContext.destination);
+
+    // Schedule the song to start playing at time:0
+    source.start(0);
+
+    // Render the song
+    offlineContext.startRendering();
+
+    // Act on the result
+    offlineContext.oncomplete = function (e) {
+      // Filtered buffer!
+      var filteredBuffer = e.renderedBuffer;
+      const filteredPeaks = getPeaksAtThreshold(
+        filteredBuffer.getChannelData(0),
+        Number(threshold),
+        filteredBuffer.sampleRate
+      );
+
+      const newBeats = filteredPeaks.map(
+        (peakIndex) => peakIndex / filteredBuffer.sampleRate
+      );
+      setBeats(newBeats);
+
+      const histogram = countIntervalsBetweenNearbyPeaks(filteredPeaks, 200);
+      const newTempoCounts = groupNeighborsByTempo(
+        histogram,
+        filteredBuffer.sampleRate
+      );
+
+      // sort data by highest count
+      const sortedTempoCounts = newTempoCounts.sort(
+        (a, b) => b.count - a.count
+      );
+      setTempoCounts(sortedTempoCounts);
+    };
+  }, [threshold, audioBuffer]);
 
   const displayTempoCounts = [...tempoCounts].slice(0, 10);
   return (
@@ -178,30 +182,42 @@ export const BeatMapperPage = observer(function BeatMapperPage() {
           </Box>
         </HStack>
       </Box>
-      <TableContainer position="absolute">
-        <Table size="sm" variant="simple">
-          <Thead>
-            <Tr>
-              <Th isNumeric>Tempo (BPM)</Th>
-              <Th isNumeric>Count</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {displayTempoCounts.map(({ tempo, count }, index) => (
-              <Fragment key={index}>
-                <Tr>
-                  <Td>
-                    <span>{tempo.toFixed(2)}</span>
-                  </Td>
-                  <Td>
-                    <span>{count}</span>
-                  </Td>
-                </Tr>
-              </Fragment>
-            ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
+      <VStack m={2}>
+        <ScalarInput
+          name="Threshold"
+          value={`${threshold}`}
+          onChange={(valueString, valueNumber) => setThreshold(valueString)}
+          step={0.01}
+        />
+
+        <Text>
+          <strong>Beats detected:</strong> {beats.length}
+        </Text>
+        <TableContainer>
+          <Table size="sm" variant="simple">
+            <Thead>
+              <Tr>
+                <Th isNumeric>Tempo (BPM)</Th>
+                <Th isNumeric>Count</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {displayTempoCounts.map(({ tempo, count }, index) => (
+                <Fragment key={index}>
+                  <Tr>
+                    <Td>
+                      <span>{tempo.toFixed(2)}</span>
+                    </Td>
+                    <Td>
+                      <span>{count}</span>
+                    </Td>
+                  </Tr>
+                </Fragment>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </VStack>
     </>
   );
 });
