22c50d74a9458f600c1e7fb6e4b43fa331fe874e
Ben Rollin <ben.rollin@clever.com>
2023-09-16 17:33:48 -0700

    work in progress

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -5,18 +5,14 @@ import { useEffect, useRef, useState } from "react";
 import { WavesurferWaveform } from "@/src/components/Wavesurfer/WavesurferWaveform";
 import { MAX_TIME } from "@/src/utils/time";
 import { TimerReadout } from "@/src/components/TimerReadout";
-import { MarkerEditorModal } from "@/src/components/MarkerEditorModal";
 import { TimerControls } from "@/src/components/TimerControls";
 import { useWheelZooming } from "@/src/hooks/wheelZooming";
 import {
   countIntervalsBetweenNearbyPeaks,
   getPeaksAtThreshold,
+  groupNeighborsByTempo,
 } from "@/src/utils/audio";
 import { runInAction } from "mobx";
-import {
-  MAX_PIXELS_PER_SECOND,
-  MIN_PIXELS_PER_SECOND,
-} from "@/src/types/UIStore";
 
 export const Test = observer(function Test() {
   const store = useStore();
@@ -32,7 +28,7 @@ export const Test = observer(function Test() {
       initialized.current = true;
 
       runInAction(() => {
-        playlistStore.experienceFilenames = ["joe-wandersail"];
+        playlistStore.experienceFilenames = ["joe-night-jar"];
       });
       store.initializeClientSide();
     }
@@ -42,7 +38,9 @@ export const Test = observer(function Test() {
 
   useEffect(() => {
     audioStore.wavesurfer?.on("ready", () => {
-      uiStore.zoomIn(300);
+      runInAction(() => {
+        uiStore.pixelsPerSecond = 20;
+      });
       const buffer = audioStore.wavesurfer!.getDecodedData()!;
       console.log("buffer", buffer);
       // Create offline context
@@ -59,6 +57,7 @@ export const Test = observer(function Test() {
       // Create filter
       var filter = offlineContext.createBiquadFilter();
       filter.type = "lowpass";
+      console.log("filter frequency", filter.frequency);
 
       // Pipe the song into the filter, and the filter into the offline context
       source.connect(filter);
@@ -77,28 +76,50 @@ export const Test = observer(function Test() {
         console.log("filteredBuffer", filteredBuffer);
         const filteredPeaks = getPeaksAtThreshold(
           filteredBuffer.getChannelData(0),
-          0.5
+          0.8,
+          filteredBuffer.sampleRate
         );
         console.log("filteredPeaks", filteredPeaks);
 
-        setBeats(
-          filteredPeaks.map(
-            (peakIndex) => peakIndex / filteredBuffer.sampleRate
-          )
+        const newBeats = filteredPeaks.map(
+          (peakIndex) => peakIndex / filteredBuffer.sampleRate
         );
-
+        setBeats(newBeats);
         const histogram = countIntervalsBetweenNearbyPeaks(filteredPeaks, 200);
-        console.log("histogram", histogram);
+        // console.log("histogram", histogram);
+
+        const bpmCounts = groupNeighborsByTempo(
+          histogram,
+          filteredBuffer.sampleRate
+        );
+        console.log("bpmCounts", bpmCounts);
 
         // sort data by highest count
-        const sorted = histogram.sort((a, b) => b.count - a.count);
+        const sorted = bpmCounts.sort((a, b) => b.count - a.count);
         console.log("sorted", sorted);
       };
     });
-  }, [audioStore.wavesurfer, audioStore.peaks]);
+  }, [audioStore.wavesurfer, audioStore.peaks, uiStore]);
 
   return (
     <>
+      <VStack
+        position="sticky"
+        top={0}
+        left={0}
+        flexShrink={0}
+        boxSizing="border-box"
+        borderRightWidth={1}
+        borderBottomWidth={1}
+        borderColor="black"
+        spacing={0}
+        width="150px"
+        zIndex={18}
+        bgColor="gray.500"
+      >
+        <TimerControls />
+        <TimerReadout />
+      </VStack>
       <Box
         ref={timelineRef}
         position="relative"
@@ -113,24 +134,6 @@ export const Test = observer(function Test() {
           spacing={0}
           zIndex={12}
         >
-          <VStack
-            position="sticky"
-            top={0}
-            left={0}
-            flexShrink={0}
-            boxSizing="border-box"
-            borderRightWidth={1}
-            borderBottomWidth={1}
-            borderColor="black"
-            spacing={0}
-            width="150px"
-            zIndex={18}
-            bgColor="gray.500"
-          >
-            <TimerControls />
-            <TimerReadout />
-          </VStack>
-
           <WavesurferWaveform />
         </HStack>
         <HStack
@@ -140,23 +143,6 @@ export const Test = observer(function Test() {
           spacing={0}
           zIndex={12}
         >
-          <VStack
-            position="sticky"
-            top={0}
-            left={0}
-            flexShrink={0}
-            boxSizing="border-box"
-            borderRightWidth={1}
-            borderBottomWidth={1}
-            borderColor="black"
-            spacing={0}
-            width="150px"
-            zIndex={18}
-            bgColor="gray.500"
-          >
-            <TimerControls />
-            <TimerReadout />
-          </VStack>
           <Box position="relative" height="100px">
             {beats.map((beat, index) => (
               <Box
--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -1,14 +1,18 @@
 // Source: http://joesul.li/van/beat-detection-using-web-audio/
 
 // Function to identify peaks
-export function getPeaksAtThreshold(data: Float32Array, threshold: number) {
+export function getPeaksAtThreshold(
+  data: Float32Array,
+  threshold: number,
+  sampleRate: number
+) {
   const peaksArray = [];
   const length = data.length;
   for (let i = 0; i < length; ) {
     if (data[i] > threshold) {
       peaksArray.push(i);
-      // Skip forward ~ 1/4s to get past this peak.
-      i += 1000;
+      // Skip forward 1/10s to get past this peak.
+      i += sampleRate / 10;
     }
     i++;
   }
@@ -25,6 +29,7 @@ export function countIntervalsBetweenNearbyPeaks(
     for (let i = 0; i < 10; i++) {
       const interval =
         Math.round((peaks[index + i] - peak) / roundFactor) * roundFactor;
+      if (interval === 0) continue;
       const foundInterval = intervalCounts.some(function (intervalCount) {
         if (intervalCount.interval === interval) return intervalCount.count++;
       });
@@ -37,4 +42,34 @@ export function countIntervalsBetweenNearbyPeaks(
     }
   });
   return intervalCounts;
+}
+
+// Function used to return a histogram of tempo candidates.
+export function groupNeighborsByTempo(
+  intervalCounts: { interval: number; count: number }[],
+  sampleRate: number
+) {
+  const tempoCounts: { tempo: number; count: number }[] = [];
+  intervalCounts.forEach(
+    (intervalCount: { interval: number; count: number }, i: number) => {
+      // Convert an interval to tempo
+      let theoreticalTempo = 60 / (intervalCount.interval / sampleRate);
+
+      // Adjust the tempo to fit within the 90-180 BPM range
+      while (theoreticalTempo < 90) theoreticalTempo *= 2;
+      while (theoreticalTempo > 180) theoreticalTempo /= 2;
+
+      const foundTempo = tempoCounts.some(function (tempoCount) {
+        if (tempoCount.tempo === theoreticalTempo)
+          return (tempoCount.count += intervalCount.count);
+      });
+      if (!foundTempo) {
+        tempoCounts.push({
+          tempo: theoreticalTempo,
+          count: intervalCount.count,
+        });
+      }
+    }
+  );
+  return tempoCounts;
 }
