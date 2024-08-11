988057a443bf6ec862e05a78d9bfd9afa37dfaf9
Ben Rollin <ben.rollin@clever.com>
2024-08-10 21:25:32 -0700

    audio script update

--- src/scripts/downloadAudio.sh
+++ src/scripts/downloadAudio.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 # Downloaded these with constant bitrate:
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 320K 'https://www.youtube.com/watch?v=CL_3mlOPnGI' -o 'concerning-hobbits.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=c7IlxIxMyMQ' -o 'crossing-mars.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=yjamrOtMKfE' -o 'the-blue-whale.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=HnhuZ_p-dbQ' -o 'the-queen-of-all-everything.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=use8dcTn4tI' -o 'ratatat-tacobelcanon.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=J-WNYs9GwKM' -o 'the-king-beetle-on-a-coconut-estate.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=A7KYhOGdagg' -o 'light-dr-toast.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=C_w9eMbGFz8' -o 'hiatus-nightjar.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=11x-CpGcqF8' -o 'vanille-fraise.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 320K 'https://www.youtube.com/watch?v=CL_3mlOPnGI' -o 'concerning-hobbits.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=c7IlxIxMyMQ' -o 'crossing-mars.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=yjamrOtMKfE' -o 'the-blue-whale.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=HnhuZ_p-dbQ' -o 'the-queen-of-all-everything.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=use8dcTn4tI' -o 'ratatat-tacobelcanon.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=J-WNYs9GwKM' -o 'the-king-beetle-on-a-coconut-estate.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=A7KYhOGdagg' -o 'light-dr-toast.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=C_w9eMbGFz8' -o 'hiatus-nightjar.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=11x-CpGcqF8' -o 'vanille-fraise.%(ext)s'
 
 # Still need to download these:
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'cloudkicker-explorebecurious.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'Dimond Saints  Howler.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'Edamame - Bask - 03 Deciduous.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'looped-kiasmos.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'Ott. - Skylon - 03 Rogue bagel.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o "Theophany - Time's End II- Majora's Mask Remixed - 05 Woods of Mystery.%(ext)s"
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'to-live-is-to-die-pulling-teeth.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'together-syence-nicole-kidman.%(ext)s'
-# youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'Tony Ann – RAIN.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'cloudkicker-explorebecurious.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'Dimond Saints  Howler.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'Edamame - Bask - 03 Deciduous.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'looped-kiasmos.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'Ott. - Skylon - 03 Rogue bagel.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o "Theophany - Time's End II- Majora's Mask Remixed - 05 Woods of Mystery.%(ext)s"
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'to-live-is-to-die-pulling-teeth.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'together-syence-nicole-kidman.%(ext)s'
+# yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 256K 'https://www.youtube.com/watch?v=...' -o 'Tony Ann – RAIN.%(ext)s'
+
