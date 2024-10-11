1c60fcd4cc5b103f199bc7ab8670be0987edc2da
Ben Rollin <ben.rollin@clever.com>
2024-10-11 01:08:16 -0700

    implement json column for testing purposes

---
+++ migrations/0001_late_warbound.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `users` ADD `extraData` text;--> statement-breakpoint
+ALTER TABLE `users` DROP COLUMN `age`;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,8 +4,8 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const usersTable = sqliteTable("users", {
   id: integer("id").primaryKey(),
   name: text("name").notNull(),
-  age: integer("age").notNull(),
   email: text("email").unique().notNull(),
+  extraData: text({ mode: "json" }),
 });
 
 export type InsertUser = typeof usersTable.$inferInsert;
