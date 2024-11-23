975a3658fee4f19a4970793782908b1d9d9905ac
Ben Rollin <ben.rollin@clever.com>
2024-11-22 23:08:37 -0800

    console log a data url image

--- src/components/Canvas/CartesianSpaceView.tsx
+++ src/components/Canvas/CartesianSpaceView.tsx
@@ -5,6 +5,8 @@ import fromTexture from "@/src/shaders/fromTexture.frag";
 import { observer } from "mobx-react-lite";
 import { useStore } from "@/src/types/StoreContext";
 import { makeVertexShader } from "@/src/shaders/vertexShader";
+import { useRenderTarget } from "@/src/hooks/renderTarget";
+import { runInAction } from "mobx";
 
 type Props = {
   renderTarget: WebGLRenderTarget;
@@ -16,6 +18,7 @@ export const CartesianSpaceView = observer(function CartesianSpaceView({
   visible,
 }: Props) {
   const store = useStore();
+  const { uiStore } = store;
   const outputMesh = useRef<Mesh>(null);
   const outputUniforms = useRef({
     u_texture: { value: renderTarget.texture },
@@ -39,6 +42,32 @@ export const CartesianSpaceView = observer(function CartesianSpaceView({
     gl.setRenderTarget(null);
     gl.render(outputMesh.current, camera);
   }, 1000);
+
+  // capture the thumbnail
+  const thumbnailRenderTarget = useRenderTarget(64, 64);
+  const buffer = new Uint8Array(64 * 64 * 4);
+  useFrame(({ gl, camera }) => {
+    if (!outputMesh.current || !uiStore.capturingThumbnail) return;
+
+    gl.setRenderTarget(thumbnailRenderTarget);
+    gl.render(outputMesh.current, camera);
+
+    gl.readRenderTargetPixels(thumbnailRenderTarget, 0, 0, 64, 64, buffer);
+
+    // Generate image via data url
+    const canvas = document.createElement("canvas");
+    canvas.width = 64;
+    canvas.height = 64;
+    const context = canvas.getContext("2d")!;
+    // Copy the pixels to a 2D canvas
+    const imageData = context.createImageData(64, 64);
+    console.log(buffer);
+    imageData.data.set(buffer);
+    context.putImageData(imageData, 0, 0);
+    console.log(canvas.toDataURL());
+
+    runInAction(() => (uiStore.capturingThumbnail = false));
+  }, 10000);
 
   return (
     <mesh ref={outputMesh}>
