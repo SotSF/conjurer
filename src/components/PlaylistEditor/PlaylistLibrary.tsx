f3dbb74888aecfc69455efd6a75d5e3333421aa5
Ben Rollin <ben.rollin@clever.com>
2024-11-29 19:59:21 -0800

    re-add selected playlist effect

--- src/components/PlaylistEditor/PlaylistLibrary.tsx
+++ src/components/PlaylistEditor/PlaylistLibrary.tsx
@@ -27,7 +27,7 @@ export const PlaylistLibrary = observer(function PlaylistLibrary() {
   const createPlaylist = trpc.playlist.savePlaylist.useMutation();
 
   const {
-    isFetching,
+    isPending,
     isError,
     data: playlists,
   } = trpc.playlist.listPlaylists.useQuery(
@@ -104,7 +104,7 @@ export const PlaylistLibrary = observer(function PlaylistLibrary() {
         All playlists
       </Switch>
       <VStack width="100%" spacing={0}>
-        {isFetching ? (
+        {isPending ? (
           <Spinner />
         ) : (
           playlists?.map((playlist) => (
