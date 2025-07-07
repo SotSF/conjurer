5df152c1447e2cfaa03d4d3659a72ad76d971657
Ben Rollin <ben.rollin@clever.com>
2025-07-06 23:52:38 -0700

    increase experience time to 30 mins

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_BLOCK_DURATION = 8;
 export const DEFAULT_VARIATION_DURATION = 2;
 export const DEFAULT_PERIOD = 5;
-export const MAX_TIME = 60 * 10; // length of the timeline in minutes
+export const MAX_TIME = 30 * 60; // length of the timeline in seconds (30 minutes)
 export const INITIAL_PIXELS_PER_SECOND = 40;
 export const MINIMUM_VARIATION_DURATION = 0.1;
