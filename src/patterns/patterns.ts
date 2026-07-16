f548c6b921316553e430149f20730ad57ca5326a
Ben Rollin <ben.rollin@clever.com>
2026-07-16 01:10:06 -0700

    And perlin2 pattern which better respects the time factor parameter

--- src/patterns/patterns.ts
+++ src/patterns/patterns.ts
@@ -1,3 +1,4 @@
+import { Perlin2 } from "@/src/patterns/Perlin2";
 import { Nebula } from "@/src/patterns/Nebula";
 import { Fireflies } from "@/src/patterns/Fireflies";
 import { Plasma } from "@/src/patterns/Plasma";
@@ -32,47 +33,48 @@ import { Perlin } from "@/src/patterns/Perlin";
 import { PulsePalette } from "@/src/patterns/PulsePalette";
 import { BloomKaleidoscope } from "@/src/patterns/BloomKaleidoscope";
 const patternFactories: Array<() => Pattern> = [
-    Nebula,
-    Fireflies,
-    Plasma,
-    Cymatics,
-    PulsePalette,
-    LogSpirals,
-    Lightwaves,
-    Barcode,
-    Clouds,
-    Disc,
-    Fire,
-    GentleRings,
-    Globules,
-    GradientCircles,
-    Lissajous,
-    Melt,
-    Perlin,
-    Pulse,
-    Rainbow,
-    GalaxyTour,
-    Starfield,
-    SpaceOdyssey,
-    SunCycle,
-    Tunnel,
-    BloomKaleidoscope,
-    Turbine,
-    Construct,
-    EasternRedbud,
-    Convergence,
-    CircleOfPipe,
-    PipeTime,
-    BoxTime,
+  Nebula,
+  Fireflies,
+  Plasma,
+  Cymatics,
+  PulsePalette,
+  LogSpirals,
+  Lightwaves,
+  Barcode,
+  Clouds,
+  Disc,
+  Fire,
+  GentleRings,
+  Globules,
+  GradientCircles,
+  Lissajous,
+  Melt,
+  Perlin,
+  Perlin2,
+  Pulse,
+  Rainbow,
+  GalaxyTour,
+  Starfield,
+  SpaceOdyssey,
+  SunCycle,
+  Tunnel,
+  BloomKaleidoscope,
+  Turbine,
+  Construct,
+  EasternRedbud,
+  Convergence,
+  CircleOfPipe,
+  PipeTime,
+  BoxTime,
 ];
 // Patterns that will not have their uniforms changed. These are used for checking what the default
 // uniform values are.
 const defaultPatterns: Pattern[] = patternFactories.map((f) => f());
 const defaultPatternMap: {
-    [key: string]: Pattern;
+  [key: string]: Pattern;
 } = {};
 for (const pattern of defaultPatterns)
-    defaultPatternMap[pattern.name] = pattern;
+  defaultPatternMap[pattern.name] = pattern;
 // Patterns that will have their uniforms updated by the pattern playground.
 const playgroundPatterns: Pattern[] = patternFactories.map((f) => f());
 export { defaultPatternMap, patternFactories, playgroundPatterns };
